6ba181cb45bd0105a9cc9f76b4fc7423fee7f89f
98KateMo <kr.molefe98@gmail.com>
2023-10-22 16:03:19 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""Lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
-import sys
+import argparse
 
+def main():
+    parser = argparse.ArgumentParser(description="List states from the database hbtn_0e_0_usa")
+    parser.add_argument("user", help="MySQL username")
+    parser.add_argument("password", help="MySQL password")
+    parser.add_argument("database", help="MySQL database name")
+    parser.add_argument("state_name", help="State name to search for")
+    
+    args = parser.parse_args()
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=args.user,
+                         passwd=args.password, db=args.database, port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
+    
+    state_name = args.state_name
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (state_name,))
     rows = cur.fetchall()
+    
     for row in rows:
         print(row)
+    
     cur.close()
     db.close()
+
+if __name__ == "__main__":
+    main()
