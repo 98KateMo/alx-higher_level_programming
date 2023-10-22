8a42e7e7e406c860d40a746879e6feb1033edd31
98KateMo <kr.molefe98@gmail.com>
2023-10-22 16:05:26 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""  must list all states from the database hbtn_0e_0_usa """
 import MySQLdb
-import argparse
+import sys
 
-def main():
-    parser = argparse.ArgumentParser(description="List states from the database hbtn_0e_0_usa")
-    parser.add_argument("user", help="MySQL username")
-    parser.add_argument("password", help="MySQL password")
-    parser.add_argument("database", help="MySQL database name")
-    parser.add_argument("state_name", help="State name to search for")
-    
-    args = parser.parse_args()
-
-    db = MySQLdb.connect(host="localhost", user=args.user,
-                         passwd=args.password, db=args.database, port=3306)
-    cur = db.cursor()
-    
-    state_name = args.state_name
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (state_name,))
-    rows = cur.fetchall()
-    
-    for row in rows:
-        print(row)
-    
-    cur.close()
-    db.close()
 
 if __name__ == "__main__":
-    main()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
