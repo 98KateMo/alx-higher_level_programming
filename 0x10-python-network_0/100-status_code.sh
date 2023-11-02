cac5cb96fbc56649e52b07d541ca8cf3bbff7aa2
98KateMo <kr.molefe98@gmail.com>
2023-11-02 09:45:37 -0700

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL, and displays only the status code of the response.
+# will send a request to a URL, and displays only the status code of the reply.
 curl -s -o /dev/null -w "%{http_code}" "$1"
